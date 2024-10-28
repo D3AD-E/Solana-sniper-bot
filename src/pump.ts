0781bb49a7ac57bb401d7f1103afb60e4ef3363d
Yevhenii <evg.gordi@gmail.com>
2024-10-28 21:18:58 +0100

    FRix

--- src/pump.ts
+++ src/pump.ts
@@ -106,7 +106,7 @@ async function subscribeToSlotUpdates() {
   stream.on('data', (data) => {
     const ins = data.transaction?.transaction?.meta?.innerInstructions;
     if (!ins) return;
-    console.log(JSON.stringify(data, null, 2));
+    console.log(JSON.stringify({ ...data.transaction }, null, 2));
     console.log(data.transaction?.transaction?.transaction?.message?.accountKeys);
     const instructionWithCurve = ins.find((x: any) => x.index === 5) ?? ins.find((x: any) => x.index === 4);
     console.log(instructionWithCurve);
