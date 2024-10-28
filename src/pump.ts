c76e3a11d8b7bf27d714e0ea169d83de1e656cfc
Yevhenii <evg.gordi@gmail.com>
2024-10-28 21:17:55 +0100

    Print

--- src/pump.ts
+++ src/pump.ts
@@ -106,7 +106,7 @@ async function subscribeToSlotUpdates() {
   stream.on('data', (data) => {
     const ins = data.transaction?.transaction?.meta?.innerInstructions;
     if (!ins) return;
-    console.dir(data);
+    console.log(JSON.stringify(data, null, 2));
     console.log(data.transaction?.transaction?.transaction?.message?.accountKeys);
     const instructionWithCurve = ins.find((x: any) => x.index === 5) ?? ins.find((x: any) => x.index === 4);
     console.log(instructionWithCurve);
