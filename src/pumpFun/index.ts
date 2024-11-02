3b7b49d335cf84f3b404997b2b993bf0f681bc77
Yevhenii <evg.gordi@gmail.com>
2024-11-02 14:15:41 +0100

    T

--- src/pumpFun/index.ts
+++ src/pumpFun/index.ts
@@ -104,7 +104,7 @@ export async function sellPump(
   const tipInstruction = SystemProgram.transfer({
     fromPubkey: wallet.publicKey,
     toPubkey: tipAccount,
-    lamports: tipAmount / 90,
+    lamports: tipAmount / 60,
   });
   const messageV0 = new TransactionMessage({
     payerKey: wallet.publicKey,
