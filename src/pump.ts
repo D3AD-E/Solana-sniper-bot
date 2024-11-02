0c05069c21032fb9416da34c74da0dec1d9e7bf6
Yevhenii <evg.gordi@gmail.com>
2024-11-02 04:20:03 +0100

    Test

--- src/pump.ts
+++ src/pump.ts
@@ -58,7 +58,7 @@ function isBuyDataOk(data: any) {
     const buyValue = new BN(reversedAmountBuffer); // Use the relevant slice for the value
     console.log('Parsed BigNumber3:', buyValue.toString());
     otherPersonBuySol = buyValue;
-    if (buyValue > 1500000000n) {
+    if (buyValue > 1000000000n) {
       logger.warn('Buy wrong');
       return false;
     } else return true;
