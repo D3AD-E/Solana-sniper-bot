7a4ce05dc46fe97789f39a6756c0a45ef70f6dcf
Yevhenii <evg.gordi@gmail.com>
2024-11-05 22:32:51 +0100

    T

--- src/pump.ts
+++ src/pump.ts
@@ -308,7 +308,6 @@ async function subscribeToSlotUpdates() {
       (x: any) => new PublicKey(x),
     );
     const pkKeysStr = pkKeys.map((x) => x.toString().toLowerCase());
-    console.log(pkKeysStr);
     if (findCommonElement(pkKeysStr, blackList)) {
       logger.warn('Blacklisted');
       return;
