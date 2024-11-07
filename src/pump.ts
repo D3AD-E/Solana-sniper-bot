ecd686d6fc39a680582473740c0b0f46f4952a19
Yevhenii <evg.gordi@gmail.com>
2024-11-07 16:42:06 +0100

    T

--- src/pump.ts
+++ src/pump.ts
@@ -545,7 +545,7 @@ async function monitorSellLogic(currentMint: string, associatedCurve: PublicKey,
   console.log(total);
   if (total === 0n) return true;
   const firstPart = total / 2n;
-  await new Promise((resolve) => setTimeout(resolve, 2800));
+  await new Promise((resolve) => setTimeout(resolve, 2500));
   await sellPump(
     wallet,
     tokenAccount.accountInfo.mint,
