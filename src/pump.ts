9dbf9d3b419ddc35d6e1e05e2dd03ae85b62e31b
Yevhenii <evg.gordi@gmail.com>
2024-11-09 15:21:44 +0100

    T

--- src/pump.ts
+++ src/pump.ts
@@ -106,7 +106,7 @@ function calculateTokenAverage() {
 async function getWalletBalance() {
   balance = await solanaConnection.getBalance(wallet.publicKey);
   console.log(BigInt(balance));
-  console.log(BigInt(balance) < BigInt(0.3 * LAMPORTS_PER_SOL));
+  console.log(BigInt(0.3 * LAMPORTS_PER_SOL));
 }
 
 function getAmountWeBuyBasedOnWalletFunds(currentBalance: number) {
